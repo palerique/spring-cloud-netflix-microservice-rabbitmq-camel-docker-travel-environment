d30a343d64a78adeb291e5ccdfc20946f373d26c
Paulo Henrique Lerbach Rodrigues <palerique@gmail.com>
2016-06-21 23:57:17 -0300

    ADD: parent pom and configuration service

---
+++ configuration-service/src/main/java/br/com/sitedoph/travelenvironment/ConfigurationServiceApplication.java
@@ -0,0 +1,18 @@
+package br.com.sitedoph.travelenvironment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * Created by ph on 6/21/16.
+ */
+@EnableConfigServer
+@SpringCloudApplication
+public class ConfigurationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigurationServiceApplication.class, args);
+    }
+
+}
